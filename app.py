f7a6f4a7fc93f469c0baec076ed5d389d64ed21a
gmx9308 <gmx9308@gmail.com>
2017-02-19 18:06:03 -0800

    improvement

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -12,7 +12,16 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
-    # @jwt_required()
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+        )
+
+
+
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name']==name, items), None)
         return {'item': item}, 200 if item is not None else 404
@@ -21,7 +30,7 @@ class Item(Resource):
         if next(filter(lambda x: x['name']==name, items), None) is not None:
             return {'message': "An item '{}' already exists".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -32,7 +41,7 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
